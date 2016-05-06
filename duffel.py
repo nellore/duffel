5620586f57a044127763a59e925f44fb6454fea2
Abhinav Nellore <anellore@gmail.com>
2016-05-06 14:23:54 -0400

    attempts constructing response manually

--- duffel.py
+++ duffel.py
@@ -6,20 +6,15 @@ Flask app for routing to Rail outputs on various hosts. Currently supports only
 Amazon Cloud Drive. Requires https://github.com/yadayada/acd_cli is authorized
 and set up as owner of shared directory.
 """
-from flask import Flask, redirect, render_template, abort, request
+from flask import Flask, redirect, render_template, abort, request, Response
 from contextlib import closing
 import subprocess
 import json
+import requests
 app = Flask(__name__)
 
 # Path to ACD CLI is hardcoded so app works on Webfaction
 _ACDCLI = '/home/verve/anaconda3/bin/acdcli'
-
-@app.before_request
-def before_request():
-    if request.method == 'HEAD':
-        request.method = 'GET'
-        return 'hello'
 
 @app.route('/')
 def duffout():
@@ -38,9 +33,7 @@ def forward(resource, identifier):
     """
     if resource == 'recount':
         try:
-            # Redirect to temp URL obtained from ACD CLI
-            return redirect(
-                        json.loads(
+            templink = json.loads(
                             subprocess.check_output(
                                 [
                                     _ACDCLI,
@@ -49,7 +42,23 @@ def forward(resource, identifier):
                             ]
                         )
                     )['tempLink']
-                )
+        except Exception as e:
+            # 404 out below
+            pass
+        else:
+            if request.method == 'HEAD':
+                # Workaround: use GET and simulate header
+                aws_response = requests.get(
+                        templink,
+                        headers={'range' : 'bytes=0-0'}
+                    )
+                return Response(
+                        headers=aws_response.headers,
+                        status=aws_response.status_code,
+                        content_type=aws_response.headers['content-type']
+                    )
+            # Redirect to temp URL obtained from ACD CLI
+            return redirect(templink)
         except Exception as e:
             # 404 out below
             pass
